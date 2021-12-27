fbff31849d0c1c81889d069c4ae55bd738361541
Ben <21268597+BenFixter@users.noreply.github.com>
2021-12-27 22:22:05 +0000

    Initial commit

---
+++ Interior Designer/Interior Designer.vb
@@ -0,0 +1,92 @@
+﻿Public Class Form1
+    'Variable'
+    Dim convertedC As Double
+    Dim userName, PN, Symbol As String
+    Dim roomWidth, roomLength, roomHeight, cConvert As Double
+    Dim SideSA, EndSA, RoofSA, WallsSA, Cost, SurfaceArea, roomPaint, UnderCoat As Double '[Side Walls Area, End Walls Area, Roof Area, Walls Surface Area, Cost, Surface Area, roomPaint, UnderCoat]
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub BTNStart_Click(sender As Object, e As EventArgs) Handles BTNStart.Click
+        'Declaring Variables'
+        userName = TXTForename.Text & " " & TXTSurname.Text
+        PN = TXTPhoneNumber.Text
+        roomLength = TXTRoomLength.Text
+        roomWidth = TXTRoomWidth.Text
+        roomHeight = TXTRoomHeight.Text
+        Symbol = ""
+
+
+
+        SideSA = (roomHeight * roomWidth) * 2
+        EndSA = (roomHeight * roomLength) * 2
+        RoofSA = roomWidth * roomLength
+        WallsSA = SideSA + EndSA
+        SurfaceArea = ((roomHeight * roomWidth) * 2) + ((roomHeight * roomLength) * 2) + (roomWidth * roomLength)
+        Cost = (roomPaint + UnderCoat) * SurfaceArea
+
+        If RBTNLuxuryQuality.Checked Then
+            roomPaint = 1.75
+        ElseIf RBTNStandardQuality.Checked Then
+            roomPaint = 1.1
+        ElseIf RBTNEconomyQuality.Checked Then
+            roomPaint = 0.45
+        Else
+            roomPaint = 0.45
+        End If
+
+        If CBUndercoat.Checked Then
+            UnderCoat = 0.3
+        Else
+            UnderCoat = 0
+        End If
+
+        If RBTNGbp.Checked Then
+            Symbol = "£"
+            Cost = (roomPaint + UnderCoat) * SurfaceArea
+            roomPaint = roomPaint
+        ElseIf RBTNYen.Checked Then
+            Cost = Cost * 142.31
+            roomPaint = roomPaint * 142.31
+            Symbol = "¥"
+        ElseIf RBTNEuros.Checked Then
+            Cost = Cost * 1.15
+            roomPaint = roomPaint * 1.15
+            Symbol = "€"
+        ElseIf RBTNDollars.Checked Then
+            Cost = Cost * 1.29
+            roomPaint = roomPaint * 1.29
+            Symbol = "$"
+        Else
+            Symbol = "£"
+            Cost = (roomPaint + UnderCoat) * SurfaceArea
+            roomPaint = roomPaint
+        End If
+        'Output'
+        MessageBox.Show("Name " & userName & vbNewLine & "Phone Number " & PN & vbNewLine & "Surface Area " & SurfaceArea & vbNewLine & "Total Cost " & Symbol & Cost & vbNewLine & "Paint Cost: " & Symbol & roomPaint)
+
+    End Sub
+
+    Private Sub BTNCon_Click(sender As Object, e As EventArgs) Handles BTNCon.Click
+        If String.IsNullOrEmpty(TXTCurvC.Text) Or String.IsNullOrWhiteSpace(TXTCurvC.Text) Then cConvert = Cost Else cConvert = TXTCurvC.Text
+
+        If RBTNGbp.Checked Then
+            convertedC = convertedC
+            convertedLab.Text = "Converted: £" & convertedC
+        ElseIf RBTNYen.Checked Then
+            convertedC = convertedC * 0.00708
+            convertedLab.Text = "Converted: £" & convertedC
+        ElseIf RBTNEuros.Checked Then
+            If String.IsNullOrWhiteSpace(Symbol) Then convertedC = convertedC * 1.15
+            convertedLab.Text = "Converted: £" & convertedC
+        ElseIf RBTNDollars.Checked Then
+            convertedC = convertedC * 1.29
+            convertedLab.Text = "Converted: £" & convertedC
+        Else
+            convertedC = convertedC
+            convertedLab.Text = "Converted: £" & convertedC
+        End If
+    End Sub
+
+
+End Class
---
+++ Interior Designer/iNTERIOR dESIGNER.Designer.vb
@@ -0,0 +1,342 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LBLForename = New System.Windows.Forms.Label()
+        Me.LBLPhoneNumber = New System.Windows.Forms.Label()
+        Me.LBLRoomLength = New System.Windows.Forms.Label()
+        Me.LBLRoomWidth = New System.Windows.Forms.Label()
+        Me.LBLRoomHeight = New System.Windows.Forms.Label()
+        Me.TXTForename = New System.Windows.Forms.TextBox()
+        Me.TXTPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.LBLSurname = New System.Windows.Forms.Label()
+        Me.TXTSurname = New System.Windows.Forms.TextBox()
+        Me.TXTRoomLength = New System.Windows.Forms.TextBox()
+        Me.TXTRoomWidth = New System.Windows.Forms.TextBox()
+        Me.TXTRoomHeight = New System.Windows.Forms.TextBox()
+        Me.RBTNLuxuryQuality = New System.Windows.Forms.RadioButton()
+        Me.RBTNStandardQuality = New System.Windows.Forms.RadioButton()
+        Me.RBTNEconomyQuality = New System.Windows.Forms.RadioButton()
+        Me.BTNStart = New System.Windows.Forms.Button()
+        Me.CBUndercoat = New System.Windows.Forms.CheckBox()
+        Me.RBTNYen = New System.Windows.Forms.RadioButton()
+        Me.RBTNEuros = New System.Windows.Forms.RadioButton()
+        Me.RBTNDollars = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RBTNGbp = New System.Windows.Forms.RadioButton()
+        Me.TXTCurvC = New System.Windows.Forms.TextBox()
+        Me.BTNCon = New System.Windows.Forms.Button()
+        Me.convertedLab = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LBLForename
+        '
+        Me.LBLForename.AutoSize = True
+        Me.LBLForename.Location = New System.Drawing.Point(12, 9)
+        Me.LBLForename.Name = "LBLForename"
+        Me.LBLForename.Size = New System.Drawing.Size(54, 13)
+        Me.LBLForename.TabIndex = 0
+        Me.LBLForename.Text = "Forename"
+        '
+        'LBLPhoneNumber
+        '
+        Me.LBLPhoneNumber.AutoSize = True
+        Me.LBLPhoneNumber.Location = New System.Drawing.Point(238, 9)
+        Me.LBLPhoneNumber.Name = "LBLPhoneNumber"
+        Me.LBLPhoneNumber.Size = New System.Drawing.Size(78, 13)
+        Me.LBLPhoneNumber.TabIndex = 1
+        Me.LBLPhoneNumber.Text = "Phone Number"
+        '
+        'LBLRoomLength
+        '
+        Me.LBLRoomLength.AutoSize = True
+        Me.LBLRoomLength.Location = New System.Drawing.Point(12, 64)
+        Me.LBLRoomLength.Name = "LBLRoomLength"
+        Me.LBLRoomLength.Size = New System.Drawing.Size(71, 13)
+        Me.LBLRoomLength.TabIndex = 2
+        Me.LBLRoomLength.Text = "Room Length"
+        '
+        'LBLRoomWidth
+        '
+        Me.LBLRoomWidth.AutoSize = True
+        Me.LBLRoomWidth.Location = New System.Drawing.Point(12, 85)
+        Me.LBLRoomWidth.Name = "LBLRoomWidth"
+        Me.LBLRoomWidth.Size = New System.Drawing.Size(66, 13)
+        Me.LBLRoomWidth.TabIndex = 3
+        Me.LBLRoomWidth.Text = "Room Width"
+        '
+        'LBLRoomHeight
+        '
+        Me.LBLRoomHeight.AutoSize = True
+        Me.LBLRoomHeight.Location = New System.Drawing.Point(12, 105)
+        Me.LBLRoomHeight.Name = "LBLRoomHeight"
+        Me.LBLRoomHeight.Size = New System.Drawing.Size(69, 13)
+        Me.LBLRoomHeight.TabIndex = 4
+        Me.LBLRoomHeight.Text = "Room Height"
+        '
+        'TXTForename
+        '
+        Me.TXTForename.Location = New System.Drawing.Point(15, 25)
+        Me.TXTForename.Name = "TXTForename"
+        Me.TXTForename.Size = New System.Drawing.Size(100, 20)
+        Me.TXTForename.TabIndex = 5
+        '
+        'TXTPhoneNumber
+        '
+        Me.TXTPhoneNumber.Location = New System.Drawing.Point(241, 25)
+        Me.TXTPhoneNumber.Name = "TXTPhoneNumber"
+        Me.TXTPhoneNumber.Size = New System.Drawing.Size(142, 20)
+        Me.TXTPhoneNumber.TabIndex = 7
+        '
+        'LBLSurname
+        '
+        Me.LBLSurname.AutoSize = True
+        Me.LBLSurname.Location = New System.Drawing.Point(114, 9)
+        Me.LBLSurname.Name = "LBLSurname"
+        Me.LBLSurname.Size = New System.Drawing.Size(49, 13)
+        Me.LBLSurname.TabIndex = 7
+        Me.LBLSurname.Text = "Surname"
+        '
+        'TXTSurname
+        '
+        Me.TXTSurname.Location = New System.Drawing.Point(117, 25)
+        Me.TXTSurname.Name = "TXTSurname"
+        Me.TXTSurname.Size = New System.Drawing.Size(100, 20)
+        Me.TXTSurname.TabIndex = 6
+        '
+        'TXTRoomLength
+        '
+        Me.TXTRoomLength.Location = New System.Drawing.Point(89, 57)
+        Me.TXTRoomLength.Name = "TXTRoomLength"
+        Me.TXTRoomLength.Size = New System.Drawing.Size(26, 20)
+        Me.TXTRoomLength.TabIndex = 8
+        '
+        'TXTRoomWidth
+        '
+        Me.TXTRoomWidth.Location = New System.Drawing.Point(89, 82)
+        Me.TXTRoomWidth.Name = "TXTRoomWidth"
+        Me.TXTRoomWidth.Size = New System.Drawing.Size(26, 20)
+        Me.TXTRoomWidth.TabIndex = 9
+        '
+        'TXTRoomHeight
+        '
+        Me.TXTRoomHeight.Location = New System.Drawing.Point(89, 105)
+        Me.TXTRoomHeight.Name = "TXTRoomHeight"
+        Me.TXTRoomHeight.Size = New System.Drawing.Size(26, 20)
+        Me.TXTRoomHeight.TabIndex = 10
+        '
+        'RBTNLuxuryQuality
+        '
+        Me.RBTNLuxuryQuality.AutoSize = True
+        Me.RBTNLuxuryQuality.Location = New System.Drawing.Point(121, 60)
+        Me.RBTNLuxuryQuality.Name = "RBTNLuxuryQuality"
+        Me.RBTNLuxuryQuality.Size = New System.Drawing.Size(263, 17)
+        Me.RBTNLuxuryQuality.TabIndex = 11
+        Me.RBTNLuxuryQuality.TabStop = True
+        Me.RBTNLuxuryQuality.Text = "Luxury quality which costs £1.75 per square metre."
+        Me.RBTNLuxuryQuality.UseVisualStyleBackColor = True
+        '
+        'RBTNStandardQuality
+        '
+        Me.RBTNStandardQuality.AutoSize = True
+        Me.RBTNStandardQuality.Location = New System.Drawing.Point(121, 83)
+        Me.RBTNStandardQuality.Name = "RBTNStandardQuality"
+        Me.RBTNStandardQuality.Size = New System.Drawing.Size(275, 17)
+        Me.RBTNStandardQuality.TabIndex = 12
+        Me.RBTNStandardQuality.TabStop = True
+        Me.RBTNStandardQuality.Text = "Standard quality which costs £1.10 per square metre."
+        Me.RBTNStandardQuality.UseVisualStyleBackColor = True
+        '
+        'RBTNEconomyQuality
+        '
+        Me.RBTNEconomyQuality.AutoSize = True
+        Me.RBTNEconomyQuality.Location = New System.Drawing.Point(120, 106)
+        Me.RBTNEconomyQuality.Name = "RBTNEconomyQuality"
+        Me.RBTNEconomyQuality.Size = New System.Drawing.Size(276, 17)
+        Me.RBTNEconomyQuality.TabIndex = 13
+        Me.RBTNEconomyQuality.TabStop = True
+        Me.RBTNEconomyQuality.Text = "Economy quality which costs £0.45 per square metre."
+        Me.RBTNEconomyQuality.UseVisualStyleBackColor = True
+        '
+        'BTNStart
+        '
+        Me.BTNStart.Location = New System.Drawing.Point(12, 155)
+        Me.BTNStart.Name = "BTNStart"
+        Me.BTNStart.Size = New System.Drawing.Size(427, 19)
+        Me.BTNStart.TabIndex = 15
+        Me.BTNStart.Text = "Start"
+        Me.BTNStart.UseVisualStyleBackColor = True
+        '
+        'CBUndercoat
+        '
+        Me.CBUndercoat.AutoSize = True
+        Me.CBUndercoat.Location = New System.Drawing.Point(120, 129)
+        Me.CBUndercoat.Name = "CBUndercoat"
+        Me.CBUndercoat.Size = New System.Drawing.Size(103, 17)
+        Me.CBUndercoat.TabIndex = 14
+        Me.CBUndercoat.Text = "Undercoat Paint"
+        Me.CBUndercoat.UseVisualStyleBackColor = True
+        '
+        'RBTNYen
+        '
+        Me.RBTNYen.AutoSize = True
+        Me.RBTNYen.Location = New System.Drawing.Point(6, 34)
+        Me.RBTNYen.Name = "RBTNYen"
+        Me.RBTNYen.Size = New System.Drawing.Size(34, 17)
+        Me.RBTNYen.TabIndex = 17
+        Me.RBTNYen.TabStop = True
+        Me.RBTNYen.Text = " ¥"
+        Me.RBTNYen.UseVisualStyleBackColor = True
+        '
+        'RBTNEuros
+        '
+        Me.RBTNEuros.AutoSize = True
+        Me.RBTNEuros.Location = New System.Drawing.Point(6, 57)
+        Me.RBTNEuros.Name = "RBTNEuros"
+        Me.RBTNEuros.Size = New System.Drawing.Size(31, 17)
+        Me.RBTNEuros.TabIndex = 18
+        Me.RBTNEuros.TabStop = True
+        Me.RBTNEuros.Text = "€"
+        Me.RBTNEuros.UseVisualStyleBackColor = True
+        '
+        'RBTNDollars
+        '
+        Me.RBTNDollars.AutoSize = True
+        Me.RBTNDollars.Location = New System.Drawing.Point(6, 83)
+        Me.RBTNDollars.Name = "RBTNDollars"
+        Me.RBTNDollars.Size = New System.Drawing.Size(31, 17)
+        Me.RBTNDollars.TabIndex = 19
+        Me.RBTNDollars.TabStop = True
+        Me.RBTNDollars.Text = "$"
+        Me.RBTNDollars.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RBTNGbp)
+        Me.GroupBox1.Controls.Add(Me.RBTNYen)
+        Me.GroupBox1.Controls.Add(Me.RBTNEuros)
+        Me.GroupBox1.Controls.Add(Me.RBTNDollars)
+        Me.GroupBox1.Location = New System.Drawing.Point(390, 16)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(43, 109)
+        Me.GroupBox1.TabIndex = 20
+        Me.GroupBox1.TabStop = False
+        '
+        'RBTNGbp
+        '
+        Me.RBTNGbp.AutoSize = True
+        Me.RBTNGbp.Location = New System.Drawing.Point(6, 11)
+        Me.RBTNGbp.Name = "RBTNGbp"
+        Me.RBTNGbp.Size = New System.Drawing.Size(31, 17)
+        Me.RBTNGbp.TabIndex = 20
+        Me.RBTNGbp.TabStop = True
+        Me.RBTNGbp.Text = "£"
+        Me.RBTNGbp.UseVisualStyleBackColor = True
+        '
+        'TXTCurvC
+        '
+        Me.TXTCurvC.Location = New System.Drawing.Point(217, 129)
+        Me.TXTCurvC.Name = "TXTCurvC"
+        Me.TXTCurvC.Size = New System.Drawing.Size(33, 20)
+        Me.TXTCurvC.TabIndex = 21
+        '
+        'BTNCon
+        '
+        Me.BTNCon.Location = New System.Drawing.Point(256, 127)
+        Me.BTNCon.Name = "BTNCon"
+        Me.BTNCon.Size = New System.Drawing.Size(60, 23)
+        Me.BTNCon.TabIndex = 22
+        Me.BTNCon.Text = "Convert"
+        Me.BTNCon.UseVisualStyleBackColor = True
+        '
+        'convertedLab
+        '
+        Me.convertedLab.AutoSize = True
+        Me.convertedLab.Location = New System.Drawing.Point(322, 133)
+        Me.convertedLab.Name = "convertedLab"
+        Me.convertedLab.Size = New System.Drawing.Size(76, 13)
+        Me.convertedLab.TabIndex = 23
+        Me.convertedLab.Text = "Converted: {0}"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(439, 181)
+        Me.Controls.Add(Me.convertedLab)
+        Me.Controls.Add(Me.BTNCon)
+        Me.Controls.Add(Me.TXTCurvC)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.CBUndercoat)
+        Me.Controls.Add(Me.BTNStart)
+        Me.Controls.Add(Me.RBTNEconomyQuality)
+        Me.Controls.Add(Me.RBTNStandardQuality)
+        Me.Controls.Add(Me.RBTNLuxuryQuality)
+        Me.Controls.Add(Me.TXTRoomHeight)
+        Me.Controls.Add(Me.TXTRoomWidth)
+        Me.Controls.Add(Me.TXTRoomLength)
+        Me.Controls.Add(Me.TXTSurname)
+        Me.Controls.Add(Me.LBLSurname)
+        Me.Controls.Add(Me.TXTPhoneNumber)
+        Me.Controls.Add(Me.TXTForename)
+        Me.Controls.Add(Me.LBLRoomHeight)
+        Me.Controls.Add(Me.LBLRoomWidth)
+        Me.Controls.Add(Me.LBLRoomLength)
+        Me.Controls.Add(Me.LBLPhoneNumber)
+        Me.Controls.Add(Me.LBLForename)
+        Me.Name = "Form1"
+        Me.Text = "Interior Designer"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LBLForename As Label
+    Friend WithEvents LBLPhoneNumber As Label
+    Friend WithEvents LBLRoomLength As Label
+    Friend WithEvents LBLRoomWidth As Label
+    Friend WithEvents LBLRoomHeight As Label
+    Friend WithEvents TXTForename As TextBox
+    Friend WithEvents TXTPhoneNumber As TextBox
+    Friend WithEvents LBLSurname As Label
+    Friend WithEvents TXTSurname As TextBox
+    Friend WithEvents TXTRoomLength As TextBox
+    Friend WithEvents TXTRoomWidth As TextBox
+    Friend WithEvents TXTRoomHeight As TextBox
+    Friend WithEvents RBTNLuxuryQuality As RadioButton
+    Friend WithEvents RBTNStandardQuality As RadioButton
+    Friend WithEvents RBTNEconomyQuality As RadioButton
+    Friend WithEvents BTNStart As Button
+    Friend WithEvents CBUndercoat As CheckBox
+    Friend WithEvents RBTNYen As RadioButton
+    Friend WithEvents RBTNEuros As RadioButton
+    Friend WithEvents RBTNDollars As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RBTNGbp As RadioButton
+    Friend WithEvents TXTCurvC As TextBox
+    Friend WithEvents BTNCon As Button
+    Friend WithEvents convertedLab As Label
+End Class
